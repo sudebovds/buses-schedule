162b8ec7ebf64c8f3880f162ca3856565d98748e
Mit Sudebov <sudebovds@gmail.com>
2024-06-04 13:48:50 +0200

    Fixed the unathorized connection to the API

--- src/helpers/apollo.tsx
+++ src/helpers/apollo.tsx
@@ -2,5 +2,10 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
     uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
-    cache: new InMemoryCache()
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'digitransit-subscription-key': '40402e35e8714b8685b6fe7b3e51a586'
+    },
+    cache: new InMemoryCache(),
 })
